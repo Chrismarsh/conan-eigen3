35de0ade5a96ec2db7a439471e5f9a4d0ab1d283
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-16 15:53:16 -0600

    remove deprecated conanfile_directory

--- test/conanfile.py
+++ test/conanfile.py
@@ -15,7 +15,7 @@ class DefaultNameConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure()
         cmake.build()
 
     def test(self):
